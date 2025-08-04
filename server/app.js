154319cecec127eb429aad508c0c20948cef71ad
ItzDJYP <130939251+ItzDJYP@users.noreply.github.com>
2025-08-04 18:48:28 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
     const express = require('express');
-    const path = require('path'); // Node.js built-in module for path manipulation
+    const path = require('path'); // Import the necessary modules
     const app = express();
     const port = 3000;
 
